0e02236136a35d1c59c17a51f17aabd5efa8a009
Suyog <suyog@example.com>
2026-04-26 20:09:00 +0530

    feat: standard URL SPA routing for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { CreatorProvider } from './context/CreatorContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/creatoriq">
       <CreatorProvider>
         <App />
       </CreatorProvider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
